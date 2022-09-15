4cb942a45bc4766b45d2fc3fd30e7f64a9b4ddb5
rafaur99 <rafael2170246@correo.uis.edu.co>
2022-09-15 12:13:39 -0500

    add archives

---
+++ src/test/java/co/edu/utp/misiontic/rafaeluribe/demosb2/DemoSb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic.rafaeluribe.demosb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
